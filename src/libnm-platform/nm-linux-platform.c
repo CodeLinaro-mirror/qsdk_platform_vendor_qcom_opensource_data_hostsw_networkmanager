NetworkManager/NetworkManager

commit dc98ab807ca89cfec9a6ec8b441088a4bed30c8f

platform: include "linux-headers" via "libnm-std-aux/nm-linux-compat.h"
We have our own copy of linux kernel headers, and we must never
directly include the corresponding versions from the system.

Avoid that, by only including the clones via "libnm-std-aux/nm-linux-compat.h"
and by including the compat wrapper header before other system headers.

src/libnm-platform/nm-linux-platform.c
@@ -7,6 +7,8 @@
 
 #include "nm-linux-platform.h"
 
+#include "libnm-std-aux/nm-linux-compat.h"
+
 #include <arpa/inet.h>
 #include <dlfcn.h>
 #include <endian.h>
@@ -23,7 +25,6 @@
 #include <linux/if_vlan.h>
 #include <linux/ip6_tunnel.h>
 #include <linux/tc_act/tc_mirred.h>
-#include <linux-headers/mptcp.h>
 #include <netinet/icmp6.h>
 #include <netinet/in.h>
 #include <net/if_arp.h>

src/libnm-platform/wifi/nm-wifi-utils-nl80211.c
@@ -9,13 +9,13 @@
 
 #include "nm-wifi-utils-nl80211.h"
 
+#include "libnm-std-aux/nm-linux-compat.h"
+
 #include <sys/ioctl.h>
 #include <net/ethernet.h>
 #include <unistd.h>
 #include <linux/nl80211.h>
 #include <linux/if.h>
-
-#include "linux-headers/nl80211-vnd-intel.h"
 
 #include "libnm-log-core/nm-logging.h"
 #include "libnm-platform/nm-netlink.h"

src/libnm-std-aux/nm-linux-compat.h
@@ -21,5 +21,7 @@
 
 #include "linux-headers/ethtool.h"
 #include "linux-headers/nl802154.h"
+#include "linux-headers/nl80211-vnd-intel.h"
+#include "linux-headers/mptcp.h"
 
 #endif /* __NM_LINUX_COMPAT_H__ */

CodeLinaro-mirror/qsdk_platform_vendor_qcom_opensource_data_hostsw_networkmanager

commit a8e4303b1083bc6f90d634c7bb1b0e13102d7ea2

platform: fix build with musl libc
Fix the following build error with musl libc:

  In file included from /usr/include/net/ethernet.h:10,
                   from ../src/libnm-platform/nm-linux-platform.c:17:
  /usr/include/netinet/if_ether.h:115:8: error: redefinition of 'struct ethhdr'
    115 | struct ethhdr {
        |        ^~~~~~
  In file included from ../src/linux-headers/ethtool.h:19,
                   from ../src/libnm-std-aux/nm-linux-compat.h:22,
                   from ../src/libnm-platform/nm-linux-platform.c:10:
  /usr/include/linux/if_ether.h:169:8: note: originally defined here
    169 | struct ethhdr {
        |        ^~~~~~

Fixes: dc98ab807ca8 ('platform: include "linux-headers" via "libnm-std-aux/nm-linux-compat.h"')

src/libnm-platform/nm-linux-platform.c
@@ -14,7 +14,6 @@
 #include <endian.h>
 #include <fcntl.h>
 #include <libudev.h>
-#include <net/ethernet.h>
 #include <linux/fib_rules.h>
 #include <linux/ip.h>
 #include <linux/if.h>

src/libnm-platform/wifi/nm-wifi-utils-nl80211.c
@@ -12,7 +12,6 @@
 #include "libnm-std-aux/nm-linux-compat.h"
 
 #include <sys/ioctl.h>
-#include <net/ethernet.h>
 #include <unistd.h>
 #include <linux/nl80211.h>
 #include <linux/if.h>

src/libnm-platform/wifi/nm-wifi-utils.h
@@ -7,8 +7,6 @@
 #ifndef __WIFI_UTILS_H__
 #define __WIFI_UTILS_H__
 
-#include <net/ethernet.h>
-
 #include "libnm-platform/nm-netlink.h"
 #include "libnm-base/nm-base.h"
 

src/libnm-platform/wpan/nm-wpan-utils.h
@@ -6,8 +6,6 @@
 #ifndef __WPAN_UTILS_H__
 #define __WPAN_UTILS_H__
 
-#include <net/ethernet.h>
-
 #include "libnm-platform/nm-netlink.h"
 
 typedef struct NMWpanUtils NMWpanUtils;